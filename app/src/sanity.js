8489f60d8046973eabaf1191dc86c5c4c25945f2
marteabra <marte.abrahams1@hotmail.com>
2022-05-25 13:12:29 +0200

    comment

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -4,5 +4,5 @@ export default sanityClient({
     projectId: 'wuh53pdh',
     dataset: 'production',
     apiVersion: '2022-05-06',
-    useCdn: true,  // til true når nettsiden publiseres
+    useCdn: true,  // change from false to true when posting to netlify
 })
